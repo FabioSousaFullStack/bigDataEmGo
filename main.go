d7a37aef339d2147421413e0d26431522fbfe9e4
Fabio Sousa <clubeteczagem@gmail.com>
2022-10-11 20:45:51 -0300

    nova dataframe

--- main.go
+++ main.go
@@ -21,9 +21,14 @@ func main() {
 	df := dataframe.ReadCSV(file)
 	fmt.Println("Serão analisados ",df.Nrow(), " jogos")
 
+	dfm := dataframe.New(
+		series.New([]string{"b", "a"}, series.String, "COL.1"),
+		series.New([]int{1, 2}, series.Int, "COL.2"),
+		series.New([]float64{3.0, 4.0}, series.Float, "COL.3"),
+	)
+
+	fmt.Println(dfm)
 	
-	teste := df.Col("S1")
-	fmt.Println(teste)
 
 	
 	
